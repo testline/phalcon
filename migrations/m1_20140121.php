26da69e3b0efac6828f91bcd7406771125fb2dfe
Sasha <alexander.vasiak@gmail.com>
2014-01-21 23:59:26 +0200

    Migrations 2

--- migrations/m1_20140121.php
+++ migrations/m1_20140121.php
@@ -2,7 +2,7 @@
 class m1_20140121 {
 
     public static function migrate($di) {
-        $r = $di->get('db')->query("ALTER TABLE `collections` ADD `brands_id` INT NOT NULL");
+//        $r = $di->get('db')->query("ALTER TABLE `collections` ADD `brands_id` INT NOT NULL");
     }
 
 }
